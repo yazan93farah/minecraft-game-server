b0f0933cbce9c0dc2366c3f7334642996b88203f
Yazan Farah <yazan93farah@gmail.com>
2025-11-21 03:08:49 +0100

    added env for some server props

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,13 @@ FROM eclipse-temurin:21-jre
 
 WORKDIR /app
 COPY server.jar .
-COPY start.sh . 
+COPY start.sh .
+
+ENV MY_MC_PORT=8888\
+    MC_MOTD="Minecraft Default Message" \
+    MC_MAX_PLAYERS="20" \
+    MC_SERVER_PORT="25565" \
+    MC_PLAYER_IDLE_TIMEOUT="10"
 RUN chmod +x start.sh
-EXPOSE 25565
+EXPOSE ${MC_SERVER_PORT}
 ENTRYPOINT ["./start.sh"]
--- start.sh
+++ start.sh
@@ -3,5 +3,18 @@
 mkdir -p server
 echo "eula=true" > server/eula.txt
 echo "EULA accepted. Starting Minecraft server..."
+
+MC_MOTD_ENV=${MC_MOTD:-"Minecraft Server"}
+MC_MAX_PLAYERS_ENV=${MC_MAX_PLAYERS:-20}
+MC_SERVER_PORT_ENV=${MC_SERVER_PORT:-25565}
+MC_PLAYER_IDLE_TIMOUT_ENV=${MC_PLAYER_IDLE_TIMOUT:-10}
+
+cat > server/server.properties <<EOF
+motd=${MC_MOTD_ENV}
+max-players=${MC_MAX_PLAYERS_ENV}
+server-port=${MC_SERVER_PORT_ENV}
+player-idle-timeout=${MC_PLAYER_IDLE_TIMOUT_ENV}
+EOF
+
 cd server
 exec java -jar ../server.jar nogui
